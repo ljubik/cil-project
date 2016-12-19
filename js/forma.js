0f6b04403c4a2a487e58ec7ff087306238faacfe
Liubomyr Kokor <ljubik@ukr.net>
2016-12-19 17:36:57 +0000

    all files from next work

---
+++ js/forma.js
@@ -0,0 +1,31 @@
+/* global $*/ //dlya togo sob $ buv v cloud 9 ok
+/* global myWindow*/
+document.getElementById('page-loaded').innerHTML =
+    (new Date()).toLocaleTimeString();
+
+$(function() {
+    $('#btn-submit').click(function(e) {
+        e.preventDefault();
+        //next code find in https://formspree.io/ > ctrl+f > $ > copy and modificate
+        $.ajax({
+            url: "https://formspree.io/reversx@gmail.com",
+            method: "POST",
+            data: {
+                Name: $('#name').val(),
+                Email: $('#email').val(), // if put "," add new dani to data and send 
+                Telefon: $('#telefon').val(),
+                Comment: $('#comment').val()
+            },
+            dataType: "json"
+        }).done(function () { //dlya perevirky ch vidpravyvsya zapyt
+            $('form').html('<h1>Thanks</h1>');
+        })
+    });
+});
+
+// yahoo YQL baze to ajax
+// add to site aend form and kurs valut, needet in more site API key
+
+function closeWin() {
+    myWindow.close();
+}
---
+++ js/index.js
@@ -0,0 +1,3 @@
+function myForm() {
+    window.open("forma.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+}
